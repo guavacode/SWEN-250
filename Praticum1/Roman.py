45d93d31d6c1703f97f3c30ab284559b9048ec63
Benjamin Handel <bah7993@rit.edu>
2014-02-13 14:30:32 -0500

    updates stuff

---
+++ Praticum1/Roman.py
@@ -0,0 +1,18 @@
+import sys
+
+def roman( entered ):
+   print(entered)
+   # the enciphered string to return 
+   enciphered = ''
+   
+   # traverse the entered string and create an enciphered string
+   # substituting characters as per the algorithm.
+
+   return enciphered
+
+def main():
+   for line in sys.stdin:
+      line = line.rstrip()
+   print(roman(line))
+
+main()
