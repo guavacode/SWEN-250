41f6982fe3dab55a693851aa135253ba38ba034f
Benjamin Handel <bah7993@rit.edu>
2014-02-13 14:59:09 -0500

    Updated some files

---
+++ Praticum-1/TestRoman.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Test One Lowercase Letter - a"
+echo "a" |  python3 Roman.py
+echo "Output should be: n"
+echo
+
+echo "Test One Lowercase Letter - n"
+echo "n" |  python3 Roman.py
+echo "Output should be: a"
+echo
+
+echo "Test original sample - SE Rocks!"
+echo "SE Rocks!" | python3 Roman.py
+echo "Output should be: FR Ebpxf!"
+
+
+#Add others here:
